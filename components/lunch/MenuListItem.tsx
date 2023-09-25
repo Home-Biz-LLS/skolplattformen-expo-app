1ca5005a279bd5af4ee838cf00abd22d948465d1
Lee <leesheppard2404@gmail.com>
2023-09-25 10:28:46 +0200

    feat(added styles to page)
    Co-authored-by: Sebastian Palmqvist <PalmN72@users.noreply.github.com>
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- components/lunch/MenuListItem.tsx
+++ components/lunch/MenuListItem.tsx
@@ -1,12 +1,30 @@
-import { Text } from '@ui-kitten/components';
+import { StyleService, Text, useStyleSheet } from '@ui-kitten/components';
 import React from 'react';
 import { View } from 'react-native';
+import { Sizing, Typography } from '../../styles';
 
 export const MenuListItem = () => {
+  const styles = useStyleSheet(themedStyles);
   return (
-    <View>
-      <Text>TEST TITLE</Text>
-      <Text>TEST DESCRIPTION</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>TEST TITLE</Text>
+      <Text category="p1">TEST DESCRIPTION</Text>
     </View>
   );
 };
+
+const themedStyles = StyleService.create({
+  container: {
+    width: '100%',
+    paddingVertical: Sizing.t3,
+  },
+  topContainer: {
+    margin: Sizing.t1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    ...Typography.header,
+    marginBottom: Sizing.t1,
+  },
+});
